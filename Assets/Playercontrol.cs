02f4adc9fcaf51deeaa752f46e87b7281d8eb6c0
Tristan Gavina <100380965+TristanGavina@users.noreply.github.com>
2023-07-06 01:03:14 +1200

    fixed sword atttack animation
    I made it so that the player does not move when they attack.
    
    I will be adding the sword hitbox next

--- Assets/Playercontrol.cs
+++ Assets/Playercontrol.cs
@@ -13,6 +13,8 @@ public class Playercontrol : MonoBehaviour
     Animator animator;
     List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
 
+    bool canMove = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -31,41 +33,59 @@ public class Playercontrol : MonoBehaviour
     }
 
     private void FixedUpdate(){
-        //If movement input is not 0, try to move
-        if(movementInput != Vector2.zero){
-            bool success = TryMove(movementInput);
-
-            if(!success) {
-                success = TryMove(new Vector2(movementInput.x, 0));
+        if (canMove) {
+            //If movement input is not 0, try to move
+            if(movementInput != Vector2.zero){
+                bool success = TryMove(movementInput);
 
                 if(!success) {
-                    success = TryMove(new Vector2(0, movementInput.y));
+                    success = TryMove(new Vector2(movementInput.x, 0));
+
+                    if(!success) {
+                        success = TryMove(new Vector2(0, movementInput.y));
+                    }
                 }
+
+                animator.SetBool("isMoving", success);
+            } else {
+                animator.SetBool("isMoving", false);
             }
-
-            animator.SetBool("isMoving", success);
-        } else {
-            animator.SetBool("isMoving", false);
         }
-    }
-    
+    }    
     private bool TryMove(Vector2 direction) {
-        // Check for potention collisions
-        int count = rb.Cast(
-            direction, // X and Y values between -1 and 1 that represent the direction from the body to look for collisions
-            movementFilter, // The settings that determine where a collision can occur on such as layers to collab with
-            castCollisions, // List of collisions to store the found collisions into after the Cast is finished
-            moveSpeed * Time.fixedDeltaTime + collisionOffset); // The amount to cast equal to the movement plus an offset
+        if(direction != Vector2.zero){
+            // Check for potention collisions
+            int count = rb.Cast(
+                direction, // X and Y values between -1 and 1 that represent the direction from the body to look for collisions
+                movementFilter, // The settings that determine where a collision can occur on such as layers to collab with
+                castCollisions, // List of collisions to store the found collisions into after the Cast is finished
+                moveSpeed * Time.fixedDeltaTime + collisionOffset); // The amount to cast equal to the movement plus an offset
 
-        if(count == 0){
-            rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
-            return true;
+            if(count == 0){
+                rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+                return true;
+            } else {
+                return false;
+            }
         } else {
+            //Cant move if theres no direction to move in
             return false;
         }
     }
 
     void OnMove(InputValue movementValue) {
         movementInput = movementValue.Get<Vector2>();
+    }
+
+    void OnFire() {
+        animator.SetTrigger("swordAttack");
+    }
+
+    public void lockMovement() {
+        canMove = false;
+    }
+
+    public void UnlockMovement() {
+        canMove = true;
     }
 }
