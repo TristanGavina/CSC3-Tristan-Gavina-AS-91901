d9c937fb3b75c35d9ca50e914ee176b752e2a926
Tristan Gavina <100380965+TristanGavina@users.noreply.github.com>
2023-07-19 09:08:55 +1200

    added a new free assets and made new areas
    I am also making the camera to be bound on 1 specific area (will be adding room transition soon)

--- Assets/Character/Scripts/Slime.cs
+++ Assets/Character/Scripts/Slime.cs
@@ -21,8 +21,7 @@ public class Slime : Enemy {
 
     void CheckDistance (){
 
-        if(Vector3.Distance(target.position,
-                            transform.position) <= chaseRadius)
+        if(Vector2.Distance(target.position, transform.position) <= chaseRadius && Vector2.Distance(target.position, transform.position) > attackRadius)
         {
             transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
         }
